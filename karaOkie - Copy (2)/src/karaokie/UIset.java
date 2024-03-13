663409e8f721cdd42d7c23fe4caa5be8feb95584
mrakkan <93730287+mrakkan@users.noreply.github.com>
2024-03-13 13:41:37 +0700

    project file

---
+++ karaOkie - Copy (2)/src/karaokie/UIset.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package karaokie;
+
+/**
+ *
+ * @author mansu
+ */
+import javax.swing.*;
+import javax.swing.plaf.basic.BasicTabbedPaneUI;
+import java.awt.*;
+public class UIset extends BasicTabbedPaneUI{
+
+    @Override
+    protected void paintTabBorder(Graphics g, int tabPlacement, int tabIndex, int x, int y, int w, int h, boolean isSelected) {
+    }
+    
+}
