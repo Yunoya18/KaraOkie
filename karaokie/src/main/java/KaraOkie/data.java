e9c97ab2b664dbd3b8727d3d905fa71132e15dd8
nuannim <66070286@kmitl.ac.th>
2024-03-28 02:36:01 +0700

    -

--- karaokie/src/main/java/KaraOkie/data.java
+++ karaokie/src/main/java/KaraOkie/data.java
@@ -3,21 +3,9 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
+
 public class data extends JPanel{
     public data(){
 
-
-    
-
-                                        
-
-
-               
+    }
 }
-}
-        
-        
-
-    
-    
-
--- karaokie/src/main/java/KaraOkie/main.java
+++ karaokie/src/main/java/KaraOkie/main.java
@@ -426,5 +426,5 @@ public class main implements ActionListener{
     }
     
     
-    }
+}
 
