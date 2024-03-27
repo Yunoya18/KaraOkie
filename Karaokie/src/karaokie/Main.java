35d6e4b714cf537e620008025b2f91bc0e4da7d2
BBoonZ <bosszaza0380@gmail.com>
2024-03-27 12:10:33 +0700

    Controller(Room + RoomMenu)

--- Karaokie/src/karaokie/Main.java
+++ Karaokie/src/karaokie/Main.java
@@ -1,8 +1,13 @@
 package karaokie;
 
+import karaokie.room.Controller;
+import java.io.*;
+
 public class Main {
+    public Controller con;
     public static void main(String[] args) {
         System.out.println("RUN!!");
+        
         new page1();
     }
 }
--- Karaokie/src/karaokie/room/Controller.java
+++ Karaokie/src/karaokie/room/Controller.java
@@ -2,15 +2,66 @@
 package karaokie.room;
 
 import java.util.*;
+import java.io.*;
 import karaokie.Menu.*;
+
 
 public class Controller{
     private static int Room_count;
+    private static ArrayList<room> room = new ArrayList<>();
     private static ArrayList<roomMenu> roomMenu = new ArrayList<>();
     
     // Socket
     
     
+    public Controller(){
+        // File
+        File file_room = new File("room.dat");
+        File file_roomMenu = new File("roomMenu.dat");
+        if (file_room.exists()) {
+            try (FileInputStream fin = new FileInputStream("room.dat"); ObjectInputStream oin = new ObjectInputStream(fin);) {
+                room = (ArrayList<room>) oin.readObject();
+
+            } catch (IOException | ClassNotFoundException e) {
+
+                e.printStackTrace();
+            }
+        }
+        if (file_roomMenu.exists()){
+            try (FileInputStream fin = new FileInputStream("roomMenu.dat"); ObjectInputStream oin = new ObjectInputStream(fin);) {
+                roomMenu = (ArrayList<roomMenu>) oin.readObject();
+
+            } catch (IOException | ClassNotFoundException e) {
+
+                e.printStackTrace();
+            }
+        } 
+    }
+
+    
+    // Room Zone
+    public static void addRoom(room r){
+        room.set(Room_count, r);
+        Room_count++;
+    }
+    
+    public static void delRoom(int index){
+        room.set(index, null);
+    }
+    
+    public static room getRoom(int index){
+        return room.get(index);
+    }
+    
+    public static void CreateSmallRoom(){
+        addRoom(new small_room(Room_count));
+    }
+    
+    public static void CreateBigRoom(){
+        addRoom(new big_room(Room_count));
+    }
+    
+    // Menu Zone
     public static void setMenu(int index, roomMenu rm){
         roomMenu.set(index, rm);
     }
@@ -23,11 +74,29 @@ public class Controller{
         setMenu(index, null);
     }
     
-    public static void CreateSmallRoom(){
-        new small_room(Room_count++);
-    }
-    
-    public static void CreateBigRoom(){
-        new big_room(Room_count++);
+    // File Save
+    public static void saveFile(){
+        
+        // room
+        try (FileOutputStream fout = new FileOutputStream("room.dat");
+            ObjectOutputStream oout = new ObjectOutputStream(fout);){
+            
+            oout.writeObject(room);
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+        
+        // roomMenu
+        try (FileOutputStream fout = new FileOutputStream("roomMenu.dat");
+            ObjectOutputStream oout = new ObjectOutputStream(fout);){
+            
+            oout.writeObject(roomMenu);
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
     }
 }
