c21ba301faaa946a1c5b6f72b95977a06b088a0c
Yunoya <sonrasamon15725@gmail.com>
2024-03-29 23:14:27 +0700

    update connection to db for login

--- Karaokie/src/database/getConnection.java
+++ Karaokie/src/database/getConnection.java
@@ -3,32 +3,60 @@ package database;
 import java.sql.*;
 
 public class getConnection {
-    public static void main(String[] args) {
-        Connection connect = null;
-        Statement statement = null;
-        String host = "jdbc:mysql://karaokie.cfc6ke0wsven.ap-southeast-2.rds.amazonaws.com/user";
-        String user = "root";
-        String password = "karaokie1234";
+    private static Connection connect = null;
+    private static Statement s = null;
+    private static final String url = "jdbc:mysql://karaokie.cfc6ke0wsven.ap-southeast-2.rds.amazonaws.com/user";
+    private static final String user = "root";
+    private static final String password = "karaokie1234";
+    public static void testConnection(){
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            connect = DriverManager.getConnection(host, user, password);
-            statement = connect.createStatement();
+            connect = DriverManager.getConnection(url, user, password);
+            s = connect.createStatement();
             String sql = "select * from user.info";
-            ResultSet rec = statement.executeQuery(sql);
+            ResultSet rec = s.executeQuery(sql);
             System.out.println("Connect Success");
             while (rec.next()) {
-                System.out.println(rec.getString("id"));
+                System.out.println(rec.getString("role"));
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
         try {
             if (connect != null) {
-                statement.close();
+                s.close();
                 connect.close();
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+    public static String getRole(String username, String pass) {
+        String sql = String.format("SELECT * FROM info WHERE id = '%s' AND password = '%s'", username, pass);
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connect = DriverManager.getConnection(url, user, password);
+            s = connect.createStatement();
+            ResultSet rec = s.executeQuery(sql);
+            while (rec.next()) {
+                return rec.getString("role");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static ResultSet getData(String sql) {
+        ResultSet rec = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connect = DriverManager.getConnection(url, user, password);
+            s = connect.createStatement();
+            rec = s.executeQuery(sql);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return rec;
     }
 }
