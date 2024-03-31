ce83e630360a2b5c73089fe53193f8e7c8a0f2dc
Yunoya <sonrasamon15725@gmail.com>
2024-03-31 23:22:45 +0700

    graph comeback jingjing leaw

--- Karaokie/src/karaokie/stat.java
+++ Karaokie/src/karaokie/stat.java
@@ -17,10 +17,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-//import org.jfree.chart.JFreeChart;
-//import org.jfree.chart.ChartFactory;
-//import org.jfree.chart.ChartPanel;
-//import org.jfree.data.category.CategoryDataset;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.data.category.CategoryDataset;
 
 public class stat extends JPanel implements ActionListener{
     private JFrame ma;
@@ -131,8 +131,8 @@ public class stat extends JPanel implements ActionListener{
 //
 //            }
 //        });
-
-        p4.add(sp);
+//
+//        p4.add(sp);
 
         //Page 5 background
         p5.setLayout(new BoxLayout(p5, BoxLayout.Y_AXIS));
@@ -154,10 +154,10 @@ public class stat extends JPanel implements ActionListener{
         p5.add(panel);
 
         // JFreeChart
-//        JFreeChart chart = ChartFactory.createBarChart("", "dd/mm/yy", "Amount", (CategoryDataset) null);
-//        ChartPanel chartPanel = new ChartPanel(chart);
-//        chartPanel.setBackground(Color.decode("#535870"));
-//        chartPanel.setPreferredSize(new Dimension(800, 550));
+        JFreeChart chart = ChartFactory.createBarChart("", "dd/mm/yy", "Amount", (CategoryDataset) null);
+        ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setBackground(Color.decode("#535870"));
+        chartPanel.setPreferredSize(new Dimension(800, 550));
 
 
         // panel in panel chart
@@ -219,7 +219,7 @@ public class stat extends JPanel implements ActionListener{
         // panel for chart
         JPanel panelChart = new JPanel();
         panelChart.setBackground(Color.decode("#535870"));
-        panelChart.add(chartPanel);
+        //panelChart.add(chartPanel);
         panelChart.add(panelChartPanel);
 
         p5.add(panelChart);
